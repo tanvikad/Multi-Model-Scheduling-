0cb7edcce65af7929c90ee4994a14fa425d88004
Dylan <ViableStream808@gmail.com>
2023-03-27 12:57:22 -0700

    clean up main code

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ model2 = MLModel("model 2", 10, 2.5, 500)
 model3 = MLModel("model 3", 15, 5, 500)
 model4 = MLModel("model 4", 2, 1, 500)
 
-sim = RoundRobin(1)
+QUANTUM = 3
+sim = RoundRobin(QUANTUM)
 sim.add_model(model1)
 sim.add_model(model2)
 sim.add_model(model3)
