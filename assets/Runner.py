96f5164ee6cde561d74cf63839d598a65f4a6682
Dylan <ViableStream808@gmail.com>
2023-03-27 12:56:06 -0700

    Fixed round robin to use quantum

--- assets/Runner.py
+++ assets/Runner.py
@@ -7,12 +7,17 @@ class RoundRobin(Simulation):
 
     def run(self, run_time: float):
         i = 0
+        q = 0
         num_model = len(self.models)
         print(self.models)
         while self.global_time < run_time:
             next = self.models[i]
             self.run_model(next)
-            i = (i + 1) % num_model
+
+            q += 1
+            if q == self.quantum:
+                i = (i + 1) % num_model
+                q = 0
         
         for line in self.logger:
             print(line)
