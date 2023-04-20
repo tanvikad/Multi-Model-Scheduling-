c94f9b083c4403ae2076cf3385296c74658f7de1
Dylan <ViableStream808@gmail.com>
2023-04-20 02:45:13 -0700

    Removed repeat code and made it easier to test simulation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from assets.scheduling_algorithm import RoundRobin, FCFS
+from assets.scheduling_algorithm import RoundRobin, FCFS, SFS, SRTF
 from assets.MLModel import MLModel
 
 model1 = MLModel("model 0", 5, 2.5, 5000)
@@ -8,15 +8,17 @@ model4 = MLModel("model 3", 2, 1, 5000)
 
 schedule = [[0, 1, 0], [1, 0, 10], [2, 0, 20], [3, 1, 20], [4, 2, 20], [5, 3, 20]]
 QUANTUM = 2
-sim = RoundRobin(schedule, QUANTUM)
+RR_sim = RoundRobin(schedule, QUANTUM)
+FCFS_sim = FCFS(schedule)
+SFS_sim = SFS(schedule)
+SRTF_sim = SRTF(schedule)
 for model in [model1, model2, model3, model4]:
-    sim.add_model(model)
-sim.run(100)
-sim.gantt_chart()
+    for simulation in [RR_sim, FCFS_sim, SFS_sim, SRTF_sim]:
+        simulation.add_model(model)
 
-# sim2 = FCFS(schedule)
-# for model in [model1, model2, model3, model4]:
-#     sim2.add_model(model)
-# sim2.run(schedule)
-# sim2.get_stats()
-# sim2.gantt_chart()
+# Change this line to change which model you want to run
+sim = SRTF_sim
+
+sim.run(100)
+sim.print_events()
+sim.gantt_chart()
